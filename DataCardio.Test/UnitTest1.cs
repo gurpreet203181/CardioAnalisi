4f03681b756bf4930836c07863a1e9a019b5196a
gs203181 <gs203181@gmail.com>
2019-04-14 17:22:16 +0200

    Punto.1 Ho scritto il metodo Unit Test per testare i metodi CalcoloFrequenzaMin e CalcoloFrequenzaMax che calcolano il numero di battiti minimi e massimi per mantenere un allenamento efficace.

--- DataCardio.Test/UnitTest1.cs
+++ DataCardio.Test/UnitTest1.cs
@@ -7,7 +7,48 @@ namespace DataCardio.Test
     public class UnitTest1
     {
 
-       
+        //punto.1   TestMethodFrequenzaMin
+        [TestMethod]
+        [DataTestMethod]
+        [DataRow(20, 140)]
+        [DataRow(33, 131)]
+        [DataRow(0, -1)]
+        [DataRow(-3, -1)]
+        [DataRow(200, -1)]
+
+
+        public void TestMethodFrequenzaMin(int età, int frequenzaMin)
+        {
+            double risposta_attesa = frequenzaMin;
+
+            double risposta = CardioanalisiLibrary.DataCardio.CalcoloFrequenzaMin(età);
+
+            Assert.AreEqual(risposta_attesa, risposta);
+
+        }
+
+        [TestMethod]
+        [DataTestMethod]
+        [DataRow(20, 180)]
+        [DataRow(33, 168)]
+        [DataRow(0, -1)]
+        [DataRow(-3, -1)]
+        [DataRow(200, -1)]
+
+        //punto.1 TestMethodFrequenzaMax
+        public void TestMethodFrequenzaMax(int età, int frequenzaMax)
+        {
+            double risposta_attesa = frequenzaMax;
+
+            double risposta = CardioanalisiLibrary.DataCardio.CalcoloFrequenzaMax(età);
+
+            Assert.AreEqual(risposta_attesa, risposta);
+
+
+
+        }
+
+
 
 
     }
