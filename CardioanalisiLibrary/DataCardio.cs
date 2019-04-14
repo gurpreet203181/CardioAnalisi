d69118443e6cf01108fd69ccbfda4b2cc93e19a3
gs203181 <gs203181@gmail.com>
2019-04-14 17:08:01 +0200

    Punto.1 Ho scritto secondo metodo che permette di calcolare il numero di battiti massimi da mantenere in un allenamento efficace.

--- CardioanalisiLibrary/DataCardio.cs
+++ CardioanalisiLibrary/DataCardio.cs
@@ -33,7 +33,29 @@ namespace CardioanalisiLibrary
 
             return frequenzaMin;
         }
-        
+
+        //Metodo per calcolare il frequenzaMax 
+        public static int CalcoloFrequenzaMax(int età)
+        {
+            int Controllo = Controlli.ControlloEta(età);//Richiamo il class controlli per controllare la età
+            int frequenzaMax = 0;
+
+            if (Controllo != -1)//se il metodo restituisce un valore int diverso da -1  significa che controlli sono andati bene.
+            {
+                int frequenzaConsigliata = 220 - età;
+
+                frequenzaMax = Convert.ToInt32(frequenzaConsigliata * 0.9);
+            }
+            else
+            {
+                frequenzaMax = -1;
+
+            }
+
+            return frequenzaMax;
+        }
+
+
 
 
 
