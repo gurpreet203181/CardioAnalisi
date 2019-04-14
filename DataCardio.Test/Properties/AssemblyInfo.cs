4915584da46fed907339e1943f878722a74d5690
gs203181 <gs203181@gmail.com>
2019-04-14 16:52:41 +0200

    Ho creato  una soluzione vuota chiamata "CardioAnalisi", all'interno della soluzione, di un progetto Libreria di classi (.NET Framework) denominato "CardioanalisiLibrary" con un file denominato DataCardio.cs e creato di un progetto di Unit Test Project (.NET Framework) chiamato DataCardio.Test

---
+++ DataCardio.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DataCardio.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DataCardio.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("46bb1223-e0b7-4e41-8b5e-63bb90ffec18")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
